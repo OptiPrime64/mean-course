5152439cd8739775066d65c076fcd5025013b811
Harold Jennett <haroldjennett@outlook.com>
2020-11-18 12:35:40 -0500

    Deployed app as one app

--- backend/app.js
+++ backend/app.js
@@ -21,22 +21,26 @@ mongoose.connect('mongodb+srv://harold:'+ process.env.MONGO_ATLAS_PW +'@cluster0
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/images", express.static(path.join("images"))); //Allows requests to images folder
+app.use("/images", express.static(path.join(__dirname, "images"))); //Allows requests to images folder
+app.use("/", express.static(path.join(__dirname, "angular")));
 
 app.get('/favicon.ico', (req, res) => res.status(204));
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, PUT, DELETE, OPTIONS' //Can also have PUT
-  )
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//   res.setHeader(
+//     'Access-Control-Allow-Methods',
+//     'GET, POST, PATCH, PUT, DELETE, OPTIONS' //Can also have PUT
+//   )
+//   next();
+// });
 
 app.use("/api/posts", postsRoutes);
 app.use("/api/user", userRoutes);
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "angular", "index.html"));
+});
 
 module.exports = app;
